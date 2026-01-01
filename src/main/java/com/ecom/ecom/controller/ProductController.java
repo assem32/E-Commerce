b0c21a37a6e03ddbe621e268867390515b6e3ef2
assem32 <assemhadidi74@gmail.com>
2026-01-01 19:57:31 +0200

    feat : add product model and init controller

---
+++ src/main/java/com/ecom/ecom/controller/ProductController.java
@@ -0,0 +1,17 @@
+package com.ecom.ecom.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+    
+    @GetMapping("/products")
+    public String getProducts() {
+        return "ff";
+    }
+    
+}
