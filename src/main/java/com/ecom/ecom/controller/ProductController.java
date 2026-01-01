71b4ca8d4fc10c9d81ed957bac2bfe32db4fff86
assem32 <assemhadidi74@gmail.com>
2026-01-01 21:52:57 +0200

    feat: add endpoint to retrieve product by ID in ProductController

--- src/main/java/com/ecom/ecom/controller/ProductController.java
+++ src/main/java/com/ecom/ecom/controller/ProductController.java
@@ -5,13 +5,17 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ecom.ecom.model.Product;
 import com.ecom.ecom.service.ProductService;
 
+import jakarta.websocket.server.PathParam;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 
 @RestController
@@ -25,5 +29,17 @@ public class ProductController {
     public ResponseEntity<List<Product> >getProducts() {
         return new ResponseEntity<>(productService.getAllProducts(),HttpStatus.ACCEPTED);
     }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable("id") int id) {
+        Product product = productService.getProductById(id);
+    
+    if (product != null) {
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    } else {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    }
+    
     
 }
